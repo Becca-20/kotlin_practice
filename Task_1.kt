2a13083cfbd06cacffef5f9b741e21de224b10f6
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2026-05-11 09:37:04 +0100

    Task_1: completed the task, want to add testing

---
+++ Task_1.kt
@@ -0,0 +1,15 @@
+// Create a function called describePerson that takes a name (String) and an age (Int). The age should have a default value of 30. The function should return a sentence like: "Alex is 25 years old."
+
+// Then call it three times:
+// - With both name and age
+// - With only a name
+// - With the age first and name second (using named arguments)
+
+fun describePerson(name: String, age: Int = 30) {
+    println("${name} is ${age} years old.")
+}
+
+fun main() {
+    describePerson("Susan", 42)
+    describePerson("Alex")
+}
